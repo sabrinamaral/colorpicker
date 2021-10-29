c82f5de9c7521560ecf3350b463d5e87210657a7
sabrinamaral <sabrinamaral@hotmail.com>
2021-10-28 22:49:57 -0300

    Functional Component && useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const colorOptions = [{name: 'Verdi Gris', hex:'#05A8AA'}, {name: 'Turquoise Green', hex: '#B8D5B8'}, {name: 'Desert Sand', hex:'#D7B49E'}, { name: 'Violet Blue Crayola', hex: '#6369D1'}, {name: 'Golden Gate Bridge', hex:'#BC412B'}];
 
-export default App;
+export default function App(props) {
+    const [ color, setColor ] = useState(colorOptions[4].hex);
+    return (
+    
+        <div className='app' style={{backgroundColor: color}}>
+            <p> Selected Color: {color}</p>
+
+            {   colorOptions.map(function(item) {
+
+                return <button onClick={()=>{
+                    setColor(item.hex)
+                }} key={item.hex}>{item.name}</button>
+                })
+            }    
+
+        </div>
+    )}
+
+    
+
+
