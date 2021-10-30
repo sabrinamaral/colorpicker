c6c9aa8416b8bcb7002cf47ea9a9f4733dd18c31
sabrinamaral <sabrinamaral@hotmail.com>
2021-10-29 22:31:10 -0300

    a tiny adjustment

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { useState } from 'react';
 
 const colorOptions = [{name: 'Verdi Gris', hex:'#05A8AA'}, {name: 'Turquoise Green', hex: '#B8D5B8'}, {name: 'Desert Sand', hex:'#D7B49E'}, { name: 'Violet Blue Crayola', hex: '#6369D1'}, {name: 'Golden Gate Bridge', hex:'#BC412B'}];
 
-export default function App(props) {
-    const [ color, setColor ] = useState(colorOptions[4].hex);
+export default function App() {
+    
+    const [ color, setColor ] = useState(colorOptions[4]);
     return (
     
-        <div className='app' style={{backgroundColor: color}}>
-            <p> Selected Color: {color}</p>
+        <div className='app' style={{backgroundColor: color.hex}}>
+            <p> Selected Color: {color.hex}</p>
 
             {   colorOptions.map(function(item) {
 
                 return <button onClick={()=>{
-                    setColor(item.hex)
+                    setColor(item)
                 }} key={item.hex}>{item.name}</button>
-                })
+            })
             }    
 
         </div>
